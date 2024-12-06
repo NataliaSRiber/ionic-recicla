9752655c949be5abd882819e1e1a331a5768a4f9
Natalia de Souza <nataliaribeiro@unifei.edu.br>
2024-12-06 10:29:02 -0300

    feat: cria o teste e a função que ao clicar no botão de login redirecione para o /home

--- recicla/src/app/pages/login/login.page.spec.ts
+++ recicla/src/app/pages/login/login.page.spec.ts
@@ -1,17 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { Router } from '@angular/router';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let router: Router
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to home page on login', () => {
+    spyOn(router, 'navigate');
+    component.login(); // função de login
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
   });
 });
--- recicla/src/app/pages/login/login.page.ts
+++ recicla/src/app/pages/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonInput, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,9 +13,13 @@ import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonIt
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
+  };
+
+  login() {
+    this.router.navigate(['home']);
+  };
 
 }
