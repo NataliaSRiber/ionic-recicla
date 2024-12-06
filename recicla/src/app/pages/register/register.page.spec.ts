a768c19ca02be7a9102ada2e2f9443d7286586ab
Natalia de Souza <nataliaribeiro@unifei.edu.br>
2024-12-06 10:53:59 -0300

    feat: cria  a função que ao clicar no botão de register redirecionar para /home

--- recicla/src/app/pages/register/register.page.spec.ts
+++ recicla/src/app/pages/register/register.page.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterPage } from './register.page';
+import { Router } from '@angular/router';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let router: Router;
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router);
   });
 
   it('should create', () => {
--- recicla/src/app/pages/register/register.page.ts
+++ recicla/src/app/pages/register/register.page.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonInput, IonItem, IonButton, IonLabel, IonRadioGroup, IonRadio } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonInput, IonItem, IonButton, IonLabel, IonRadioGroup, IonRadio, IonButtons, IonBackButton, IonNav } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonRadio, IonRadioGroup, IonLabel, IonButton, IonItem, IonInput, IonCardTitle, IonCardContent, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBackButton, IonButtons, IonRadio, IonRadioGroup, IonLabel, IonButton, IonItem, IonInput, IonCardTitle, IonCardContent, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
-  }
 
+  }
+  home() {
+    this.router.navigate(['home']);
+  }
 }
