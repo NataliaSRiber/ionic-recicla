df7d9338e96823c3b4020a7665935eda54a28ba9
Natalia de Souza <nataliaribeiro@unifei.edu.br>
2024-12-18 16:29:58 -0300

    feat: cria o form no onInit

--- recicla/src/app/pages/login/login.page.ts
+++ recicla/src/app/pages/login/login.page.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonItem, IonInput, IonLabel, IonButton } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
+import { LoginPageForm } from './login.page.form';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonButton, IonLabel, IonInput, IonItem, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonContent, CommonModule, FormsModule]
+  imports: [IonButton, IonLabel, IonInput, IonItem, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonContent, CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class LoginPage implements OnInit {
+  form!: FormGroup;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = new LoginPageForm(this.formBuilder).createForm();
   };
 
   login() {
